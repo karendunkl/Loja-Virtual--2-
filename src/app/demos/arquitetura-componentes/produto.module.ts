a6c8937e3f4efd9d887fe1c0861d675408b2784c
Karen Cristina de Brito Dunkl <ava334@einstein.br>
2021-12-28 10:28:07 -0300

    Componente Produto

--- src/app/demos/arquitetura-componentes/produto.module.ts
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -1,12 +1,19 @@
 import { ProdutoRoutingModule } from './produto.route';
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
+import { ProdutoDetalheComponent } from './componentes/produto-card-detalhe.component';
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+import { ProdutoCountComponent } from './componentes/produto-count.component';
+registerLocaleData(localePt);
 
 @NgModule({
     declarations: [
-        ProdutoDashboardComponent
+        ProdutoDashboardComponent,
+        ProdutoDetalheComponent,
+        ProdutoCountComponent
     ],
     imports: [
         CommonModule,
